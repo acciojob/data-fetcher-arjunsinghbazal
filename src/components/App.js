5346ea31f3d78508f2fa0c83be5523fbd9d47bc7
Arjun Singh <55157623+arjunsinghbazal@users.noreply.github.com>
2023-08-30 11:24:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,46 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
+import axios from 'axios'
+
 
 const App = () => {
+  let [data, setData] = useState(null);
+  let [err, setErr] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get('https://dummyjson.com/products');
+        console.log(response.data);
+        setData(response.data);
+        if (!response.data) {
+          setErr('No data found');
+          return;
+        }
+        setErr('');
+      }
+      catch (err) {
+        console.log(err);
+        setErr(`An error occurred: ${err}`);
+      }
+    })();
+  }, [])
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      {
+        data ?
+          <div>
+            <h1>Data Fetched from API</h1>
+            {
+                <pre>{JSON.stringify(data, null, 2)},</pre>
+            }
+          </div> : !err ?
+            <p>Loading...</p> : <p>{err}</p>
+      }
+
     </div>
   )
 }
